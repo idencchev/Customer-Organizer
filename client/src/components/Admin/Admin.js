e1efc02631031e342fa1d1ae91c330e0dede23e9
idenccev <idenchev@gmail.com>
2022-02-19 03:40:43 +0200

    css admin panel

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -50,39 +50,37 @@ function Admin() {
     }
 
     const onDeleteHandler = async (id) => {
-       const delUser = await deleteUser(id);
-       setusersData(usersData.filter(usersData => usersData._id !== id));
-       console.log(delUser);
+        const delUser = await deleteUser(id);
+        setusersData(usersData.filter(usersData => usersData._id !== id));
+        console.log(delUser);
     }
 
     return (
         <div className="admin">
-            <form onSubmit={onCreateHandler}>
-                <div className="admin-container">
-                    <h1>CREATE USER</h1>
-                    <label htmlFor="uname"><b>USERNAME</b></label>
-                    <input onChange={onChangeHandler} type="text" placeholder="Enter Username" name="username" required />
+            <form className="register-form" onSubmit={onCreateHandler}>
+                <h1 className="create-user">CREATE USER</h1>
+                <label htmlFor="uname"><b>USERNAME</b></label>
+                <input onChange={onChangeHandler} type="text" placeholder="Enter Username" name="username" required />
 
-                    <label htmlFor="psw"><b>PASSWORD</b></label>
-                    <input onChange={onChangeHandler} type="password" placeholder="Enter Password" name="password" required />
+                <label htmlFor="psw"><b>PASSWORD</b></label>
+                <input onChange={onChangeHandler} type="password" placeholder="Enter Password" name="password" required />
 
-                    <label htmlFor="psw"><b>REPEAT PASSWORD</b></label>
-                    <input onChange={onChangeHandler} type="password" placeholder="Repeat Password" name="rePass" required />
+                <label htmlFor="psw"><b>REPEAT PASSWORD</b></label>
+                <input onChange={onChangeHandler} type="password" placeholder="Repeat Password" name="rePass" required />
 
-                    <label htmlFor="admin">Admin Account?</label>
-                    <input onChange={isAdminHandler} type="checkbox" id="isAdmin" name="isAdmin" />
-                    <button className="btn-submit" type="submit">CREATE USER</button>
-                </div>
+                <label className="label-isAdmin" htmlFor="isAdmin">Admin Account?</label>
+                <input className="input-isAdmin" onChange={isAdminHandler} type="checkbox" id="isAdmin" name="isAdmin" />
+                <button className="btn-submit-create" type="submit">CREATE USER</button>
             </form>
             <div className="user-list-div">
-                <h2>USER'S LIST</h2>
+                <h2 className="user-list-h2">USER'S LIST</h2>
                 <ul className="user-list-ul">
                     {usersData.map(user => {
                         return (
                             <li key={user._id} className="user-list-li">
                                 {user.isAdmin ? 'Admin Account: ' : 'User Account: '}
-                                {user.username}
-                                {user._id !== id ? <button onClick={() => onDeleteHandler(user._id)} className="btn-delete-user">Delete</button> : null}
+                                {user.username + ' ' }   
+                                {user._id !== id ? <button onClick={() => onDeleteHandler(user._id)} className="btn-delete-user">DELETE</button> : null}
                             </li>
                         )
                     })}
